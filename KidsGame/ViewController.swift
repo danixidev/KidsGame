b6a793a248b198422835ed348f8762d78f6e23de
Dani <danixidev@gmail.com>
2021-10-23 22:04:32 -0700

    Added logic to create random icon, assign it and test if it matches with the button pressed. Added score label Added constraints properly

--- KidsGame/ViewController.swift
+++ KidsGame/ViewController.swift
@@ -2,12 +2,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var iconReferenceImage: UIButton!
+    
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        iconReferenceImage.setTitle(createRandomIcon(), for: .normal)
     }
 
+    
+    func createRandomIcon() -> String {
+        let icons = ["✡️", "☸️", "⚛️", "☪️", "☯️", "♒️" ]
+        let generatedNumber = Int.random(in: 0...icons.count-1)
+        
+        return icons[generatedNumber]
+    }
 
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let result = compareText(text1: sender.title(for: .normal)!, text2: iconReferenceImage.title(for: .normal)!)
+        
+        if(result) {
+            print("ha pulsado el boton bien")
+        } else {
+            print("Ha pulsado el boton mal")
+        }
+    }
+    
+    func compareText(text1: String, text2: String) -> Bool {
+        if text1 == text2 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func actualizeScore() {
+        
+    }
+    
 }
 
