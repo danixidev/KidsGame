27fe6e913cd36c2c7211f4799804e0613be03c28
Dani <danixidev@gmail.com>
2021-10-23 22:42:29 -0700

    Added all buttons to an array Added randomisation of buttons

--- KidsGame/ViewController.swift
+++ KidsGame/ViewController.swift
@@ -5,10 +5,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var iconReferenceButton: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
     
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+
+    
     var score = 0
+    var buttons: [UIButton] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        buttons.append(button1)
+        buttons.append(button2)
+        buttons.append(button3)
+        buttons.append(button4)
+        buttons.append(button5)
+        buttons.append(button6)
         
         iconReferenceButton.setTitle(createRandomIcon(), for: .normal)
         setScore(score)
@@ -35,6 +51,9 @@ class ViewController: UIViewController {
             // Create a new icon
             iconReferenceButton.setTitle(createRandomIcon(), for: .normal)
             
+            // Rearrange buttons
+            randomizeButtons()
+            
         } else {
             print("The button has been pressed incorrectly")
             
@@ -43,6 +62,9 @@ class ViewController: UIViewController {
             
             // Create a new icon
             iconReferenceButton.setTitle(createRandomIcon(), for: .normal)
+            
+            // Rearrange buttons
+            randomizeButtons()
         }
     }
     
@@ -64,6 +86,26 @@ class ViewController: UIViewController {
     
     func setScore(_ score: Int) {
         scoreLabel.text = "\(score) points"
+    }
+    
+    
+    func randomizeButtons() {
+        var icons = ["✡️", "☸️", "⚛️", "☪️", "☯️", "♒️" ]
+        
+        if icons.count == buttons.count {
+            for button in buttons {
+                // Generate random number to select icon
+                let generatedNumber = Int.random(in: 0...icons.count-1)
+                
+                // Set the icon
+                button.setTitle(icons[generatedNumber], for: .normal)
+                
+                // Delete the icon from the array
+                icons.remove(at: generatedNumber)
+            }
+        } else {
+            print("icons' and buttons' length doesnt match [randomizeButtons()]")
+        }
     }
 }
 
